ca33034e0ba5f15afb55957952dcc8aa0c16b33f
Yoann <maulineyoann@yahoo.fr>
2022-02-01 00:48:09 +0100

    footer

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,17 +1,23 @@
+
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.scss'
 
 const Footer = () => {
-  const styleFooter = {
-    height: '50px',
-    padding: '10px',
-    backgroundColor: '#00171f',
-    color: '#fff',
-    margin: 'auto',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  };
-  return <footer style={styleFooter}>yoann @copyright</footer>;
+  
+  return <footer>
+    <h3>Sneakers Addict</h3>
+    <div className="contact">
+      <h5>Navigation</h5>
+      <a href="https://www.linkedin.com/in/yoann-mauline/">Linkedin</a>
+      <a href="https://github.com/MaulineYoann">Github</a>
+    </div>
+    <div className="navigation">
+      <h5>Produit</h5>
+    <Link to="/">Acceuil</Link>
+    <Link to="/sneakers">Sneakers</Link>
+    </div>
+    </footer>;
 };
 
 export default Footer;
