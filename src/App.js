b418dd5a28c505138d8af3484c9ea3bc87b862c5
Yoann <maulineyoann@yahoo.fr>
2022-02-07 23:58:24 +0100

    responsive

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
 import Home from './pages/Home/Home';
 import Navbar from './pages/Navbar/Navbar';
-import Loader from './components/Loader/Loader';
 import SneakersID from './components/SneakersID/SneakersID';
 import NoMatch from './components/NoMatch/NoMatch';
 import Footer from './pages/Footer/Footer';
+import Loader from './components/Loader/Loader'
 import Sneakers from './components/Sneakers/Sneakers';
 import './App.scss';
-import Contact from './components/Contact/Contact';
 
 function App() {
   
   const API = 'https://a.nacapi.com/sneakers91';
   const [sneakers, setSneakers] = useState([]);
   const [loader, setLoader] = useState(true);
+
+  // useEffect(() => {
+  //   axios
+  //   .get(API)
+  //   .then((res) => res.data)
+  //   .then((data) => setSneakers(data))
+  //   console.log(sneakers);
+  // },[])
+
   const getSneakersRequest = async () => {
-    await axios
+   await axios
     .get(API)
     .then((res) => res.data)
     .then((data) => setSneakers(data));
@@ -29,6 +37,8 @@ function App() {
   return loader ? (
     <Loader />
   ) : (
+
+    // return (
     <>
       <BrowserRouter>
         <Navbar />
@@ -36,7 +46,6 @@ function App() {
           <Route path="/" element={<Home sneakers={sneakers} />} />
           <Route path="sneakers" element={<Sneakers sneakers={sneakers} />} />
           <Route path="/sneakers/:sneakerId" element={<SneakersID sneakers={sneakers} />} />
-          {/* <Route path="/contact" element={<Contact sneakers={sneakers}/>} /> */}
           <Route path="/no-match" element={<NoMatch />} />
         </Routes>
         <Footer />
